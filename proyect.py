b29acc343d5d27e8fa3686dffa861d1953f18be5
David Hernández <dh720443@gmail.com>
2026-05-02 19:52:12 -0400

    updated_proyect.py
    Improvements in error handling and variable renaming for better code understanding.
    
    Mejoras en manejo de errores y cambio de nombres de variables para mejor entendimiento del codigo.

--- proyect.py
+++ proyect.py
@@ -1,34 +1,36 @@
 import requests
 import config 
 
-url = f"https://v6.exchangerate-api.com/v6/{config.API_KEY}/latest/USD"
+try:
+  url = f"https://v6.exchangerate-api.com/v6/{config.API_KEY}/latest/USD"
+  response = requests.get(url)
+  data = response.json()
+  api_currency = data['conversion_rates']
+except AttributeError:
+  print("ERROR: The config.py file with the API key is not found!")
 
-response = requests.get(url)
-data = response.json()
-coin1 = data['conversion_rates']
-
-class Coin:
-  def __init__(self, user, coin, wallet=0):
-    self.user = user
-    self.coin = coin
-    self.wallet = wallet
+class Wallet:
+  def __init__(self, user_money, currency, wallet=0):
+    self.user_money = user_money
+    self.currency = currency
+    self.__wallet = wallet
 	
   def see_wallet(self):
-    print(f'You have USD {self.wallet:.2f} in the Wallet')
+    print(f'You have USD {self.__wallet:.2f} in the Wallet')
     input('\nPress enter to return to the menu')
 	 
   def user_wallet(self):
     user_action  = int(input('Select your needed action (1. Add USD to the wallet, 2. Withdraw USD of the wallet): '))
     if user_action == 1:
-      self.user = float(input('How much USD you want to add?: '))
-      self.wallet += self.user
-      print(f'Successfully added! your wallet now has USD {self.wallet:.2f}')
+      self.user_money = float(input('How much USD you want to add?: '))
+      self.__wallet += self.user_money
+      print(f'Successfully added! your wallet now has USD {self.__wallet:.2f}')
       input('\nPress enter to return to the menu') 
     elif user_action == 2:
-      self.user = float(input('How much USD you want to withdraw?: '))
-      if self.user <= self.wallet:
-        self.wallet -= self.user
-        print(f'Successfully withdrawn! your wallet now has USD {self.wallet:.2f}')
+      self.user_money = float(input('How much USD you want to withdraw?: '))
+      if self.user_money <= self.__wallet:
+        self.__wallet -= self.user_money
+        print(f'Successfully withdrawn! your wallet now has USD {self.__wallet:.2f}')
         input('\nPress enter to return to the menu')
       else:
         print('Error, insufficient USD in Wallet') 
@@ -36,53 +38,55 @@ class Coin:
     else:
       print('Error, invalid action') 
       input('\nPress enter to return to the menu') 
-    return self.wallet
+    return self.__wallet
 		
   def user_input(self):
     try:
-      self.coin = input('Which currency you want to convert your USD?(Ex. VES, EUR, CNY, etc): ').upper()
+      self.currency = input('Which currency you want to convert your USD? (Ex. VES, EUR, CNY, etc): ').upper()
 		
-      if self.coin in coin1:
-        final_coin = self.wallet * coin1[self.coin]
-        print(f'Your USD {self.wallet:.2f} is {self.coin} {final_coin:.3f} right now!')
+      if self.currency in api_currency:
+        final_coin = self.__wallet * api_currency[self.currency]
+        print(f'Your USD {self.__wallet:.2f} is {self.currency} {final_coin:.3f} right now!')
         input('\nPress enter to return to the menu') 
       
       else:
         print('Error, please select a valid currency')
         input('\nPress enter to return to the menu') 
-        
-    except:
-      input('Unexpected error, press enter to return the menu')							
+    except NameError:
+      input("API Error, press enter to return the menu")							
 
 print('---Welcome to your Online Wallet!---')
 
-user_money = 0
-user_coin = ''
+user_usd = 0
+user_currency = ''
 user_add = 0
-money = Coin(user_add, user_coin, user_money)
+money = Wallet(user_add, user_currency, user_usd)
 
 while True:
-  print('Please input your needed action (1, 2, 3, 4.)')
-  print('1. See Wallet Balance')
-  print('2. Add/Withdraw USD from the Wallet')
-  print('3. See your Wallet USD as other coins')
-  print('4. Exit')
+  print('''
+Please input your needed action (1, 2, 3, 4):
+
+|1. See Wallet Balance
+|2. Add/Withdraw USD from the Wallet
+|3. See your Wallet USD as other badges
+|4. Exit
+''')
   
-  user_action = input("Select an option: ").strip()
+  user_option = input("Select an option: ").strip()
   
-  if user_action == '1':
+  if user_option == '1':
     money.see_wallet()
     
-  elif user_action == '2':
+  elif user_option == '2':
     money.user_wallet()
     
-  elif user_action == '3':
+  elif user_option == '3':
     money.user_input()
     
-  elif user_action == '4':
+  elif user_option == '4':
     print('Session closed')
     break 
     
   else:
     print("Invalid option, try again")
-    input('\nPress enter to return to the menu') 
+    input('\nPress enter to return to the menu')
